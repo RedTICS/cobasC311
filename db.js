766cb996f8f81084fb8a7ec7e2fe6ddce273ff31
Luis Parada <luisparadawagner@gmail.com>
2017-07-05 14:14:39 -0300

    Valido si el protocolo tiene letras

--- db.js
+++ db.js
@@ -5,37 +5,38 @@ var logger = require('winston');
 var sql = require("seriate");
 
 // SQL Server config settings
-var dbConfig = {  
+var dbConfig = {
     "server": config.dbServer,
     "user": config.dbUser,
     "password": config.dbPassword,
     "database": config.dbDatabase
 };
 
-sql.setDefaultConfig( dbConfig );
+sql.setDefaultConfig(dbConfig);
 
+function isString(value) { return typeof value === 'string'; }
 
-function saveResult(result,order){
+function saveResult(result, order) {
     var logTime = new Date();
     var tipoMuestra = "Suero/Plasma";
-    switch (parseInt(order.biomaterial)){ 
-            case 1: tipoMuestra="Suero/Plasma";break; // TODO Colocar los Prefijos del tipo de muestra correctos
-            case 2: tipoMuestra="Orina";break;
-            case 3: tipoMuestra="CSF";break;
-            case 4: tipoMuestra="Suprnt";break;
-            case 5: tipoMuestra="Otros";break;
-        }
-	
-var sampleId = order.sampleId;
-var sampleIdSinBlancos = sampleId.trim();
-var sampleProtocolo='';
-var sampleSector = '';
+    switch (parseInt(order.biomaterial)) {
+        case 1: tipoMuestra = "Suero/Plasma"; break; // TODO Colocar los Prefijos del tipo de muestra correctos
+        case 2: tipoMuestra = "Orina"; break;
+        case 3: tipoMuestra = "CSF"; break;
+        case 4: tipoMuestra = "Suprnt"; break;
+        case 5: tipoMuestra = "Otros"; break;
+    }
 
-for (var i = 0; i < sampleIdSinBlancos.length; i++) {
+    var sampleId = order.sampleId;    
+    var sampleIdSinBlancos = (isString(sampleId)) ? sampleId.trim() : sampleId;
+    var sampleProtocolo = '';
+    var sampleSector = '';
+
+    for (var i = 0; i < sampleIdSinBlancos.length; i++) {
         let elem = sampleIdSinBlancos.charAt(i);
         switch (elem) {
             case '0':
-            case '1': 
+            case '1':
             case '2':
             case '3':
             case '4':
@@ -44,105 +45,104 @@ for (var i = 0; i < sampleIdSinBlancos.length; i++) {
             case '7':
             case '8':
             case '9':
-                    sampleProtocolo = sampleProtocolo + elem;
-                    break;
-                        
+                sampleProtocolo = sampleProtocolo + elem;
+                break;
+
             default:
-                    sampleSector = sampleSector + elem;
+                sampleSector = sampleSector + elem;
         }
-}  
-	
-	var queryModificada;
-	//Fix para cuando tienen los protocolos divididos por sectores
-	if (!isNaN(sampleSector)){
-		sampleId = parseInt(sampleIdSinBlancos);
-		queryModificada = "SELECT TOP 1 idProtocolo FROM LAB_Protocolo WHERE numeroSector = @_sampleId AND baja=0 AND estado<2";
-	}else
-	{
-		sampleId = parseInt(sampleProtocolo);
-		queryModificada = "SELECT TOP 1 idProtocolo FROM LAB_Protocolo WHERE numeroSector = @_sampleId AND prefijoSector = @_sampleSector AND baja=0 AND estado<2";
-	}
-	
-	//console.log('Esta es la query:', queryModificada);
+    }
+
+    var queryModificada;
+    //Fix para cuando tienen los protocolos divididos por sectores
+    if (!isNaN(sampleSector)) {
+        sampleId = parseInt(sampleIdSinBlancos);
+        queryModificada = "SELECT TOP 1 idProtocolo FROM LAB_Protocolo WHERE numero = @_sampleId AND baja=0 AND estado<2";
+    } else {
+        sampleId = parseInt(sampleProtocolo);
+        queryModificada = "SELECT TOP 1 idProtocolo FROM LAB_Protocolo WHERE numeroSector = @_sampleId AND prefijoSector = @_sampleSector AND baja=0 AND estado<2";
+    }
+
+    //console.log('Esta es la query:', queryModificada);
     sql.getPlainContext()
-        .step( "queryProtocoloById", {
-                query: queryModificada,
+        .step("queryProtocoloById", {
+            query: queryModificada,
+            params: {
+                _sampleId: { type: sql.INT, val: sampleId },
+                _sampleSector: { type: sql.NVARCHAR, val: sampleSector }
+            }
+        })
+        .step("queryCobasC311WithBiomaterial", function (execute, data) {
+            execute({
+                query: "SELECT TOP 1 * FROM LAB_CobasC311 WHERE idItemCobas = @_idItemCobas AND tipoMuestra = @_tipoMuestra",
                 params: {
-                    _sampleId: { type: sql.INT, val: sampleId },
-					_sampleSector: {type: sql.NVARCHAR, val: sampleSector}
+                    _idItemCobas: { type: sql.INT, val: result.test },
+                    _tipoMuestra: { type: sql.NVARCHAR, val: tipoMuestra }
                 }
-            } )
-        .step( "queryCobasC311WithBiomaterial", function( execute, data ) {
-                execute( {
-                    query: "SELECT TOP 1 * FROM LAB_CobasC311 WHERE idItemCobas = @_idItemCobas AND tipoMuestra = @_tipoMuestra",
-                    params: {
-                        _idItemCobas: { type: sql.INT, val: result.test },
-                        _tipoMuestra: { type: sql.NVARCHAR, val: tipoMuestra }
-                    }
-                } );
-                
-             } )
-         .step( "queryCobasC311WithPrefijoTipoMuestra", function( execute, data ) {
-                execute( {
-                    query: "SELECT TOP 1 * FROM LAB_CobasC311 WHERE idItemCobas = @_idItemCobas AND prefijo = @_prefijo AND tipoMuestra = @_tipoMuestra",
-                    params: {
-                        _idItemCobas: { type: sql.INT, val: result.test },
-                        _prefijo: { type: sql.NVARCHAR, val: order.prefijoTipoMuestra },
-                        _tipoMuestra: { type: sql.NVARCHAR, val: tipoMuestra }
-                    }
-                } );
-            } )
-        .end( function( sets ){
-                if (!sets.queryProtocoloById[0]){
-                    errMessage = 'No se encontro el protocolo especificado con id:' + order.sampleId;
+            });
+
+        })
+        .step("queryCobasC311WithPrefijoTipoMuestra", function (execute, data) {
+            execute({
+                query: "SELECT TOP 1 * FROM LAB_CobasC311 WHERE idItemCobas = @_idItemCobas AND prefijo = @_prefijo AND tipoMuestra = @_tipoMuestra",
+                params: {
+                    _idItemCobas: { type: sql.INT, val: result.test },
+                    _prefijo: { type: sql.NVARCHAR, val: order.prefijoTipoMuestra },
+                    _tipoMuestra: { type: sql.NVARCHAR, val: tipoMuestra }
+                }
+            });
+        })
+        .end(function (sets) {
+            if (!sets.queryProtocoloById[0]) {
+                errMessage = 'No se encontro el protocolo especificado con id:' + order.sampleId;
+                logger.error(errMessage);
+                logMessages(errMessage, logTime);
+                throw new Error(errMessage);
+            }
+
+            var idProtocolo = sets.queryProtocoloById[0].idProtocolo;
+            var idItem = "";
+
+            if (sets.queryCobasC311WithPrefijoTipoMuestra[0]) {
+                idItem = sets.queryCobasC311WithPrefijoTipoMuestra[0].idItemSil;
+
+            }
+            else {
+                if (sets.queryCobasC311WithBiomaterial[0]) {
+                    idItem = sets.queryCobasC311WithBiomaterial[0].idItemSil;
+                }
+                else {
+                    errMessage = 'No se encontro el subItem especificado con idItemCobas:' + result.test + ' y tipoMuestra:' + tipoMuestra;
                     logger.error(errMessage);
-                    logMessages(errMessage,logTime);
-                    throw new Error(errMessage);
+                    logMessages(errMessage, logTime);
                 }
-                
-                var idProtocolo = sets.queryProtocoloById[0].idProtocolo;
-                var idItem = "";
-                
-                if (sets.queryCobasC311WithPrefijoTipoMuestra[0]){
-                    idItem = sets.queryCobasC311WithPrefijoTipoMuestra[0].idItemSil;
-                    
-                }
-                else{
-                    if (sets.queryCobasC311WithBiomaterial[0]){
-                         idItem = sets.queryCobasC311WithBiomaterial[0].idItemSil;
+            }
+
+            if (idItem !== "") {
+                sql.execute({
+                    query: "UPDATE LAB_DetalleProtocolo set resultadoNum = @_resultadoNum, unidadMedida = @_unidadMedida, conResultado=1, enviado=2, fechaResultado= @_fechaResultado" +
+                    " WHERE idProtocolo= @_idProtocolo AND idSubItem= @_idSubItem AND idUsuarioValida=0",
+                    params: {
+                        _resultadoNum: { type: sql.REAL, val: result.value },
+                        _fechaResultado: { type: sql.DATETIME, val: order.dateTimeReported },
+                        _unidadMedida: { type: sql.NVARCHAR, val: result.units },
+                        _idProtocolo: { type: sql.INT, val: idProtocolo },
+                        _idSubItem: { type: sql.INT, val: idItem }
                     }
-                    else{
-                        errMessage = 'No se encontro el subItem especificado con idItemCobas:' + result.test + ' y tipoMuestra:' + tipoMuestra ;
-                        logger.error(errMessage);
-                        logMessages(errMessage,logTime);
-                    }
-                }
-                
-                if (idItem !== ""){
-                    sql.execute( {
-                        query: "UPDATE LAB_DetalleProtocolo set resultadoNum = @_resultadoNum, unidadMedida = @_unidadMedida, conResultado=1, enviado=2, fechaResultado= @_fechaResultado" +
-                                " WHERE idProtocolo= @_idProtocolo AND idSubItem= @_idSubItem AND idUsuarioValida=0",
-                        params: {
-                            _resultadoNum: { type: sql.REAL, val: result.value },
-                            _fechaResultado: { type: sql.DATETIME, val: order.dateTimeReported },
-                            _unidadMedida: { type: sql.NVARCHAR, val: result.units },
-                            _idProtocolo: { type: sql.INT, val: idProtocolo },
-                            _idSubItem: { type: sql.INT, val: idItem }
-                        }
-                    } );
-                    logger.info(result.value)
-                    logger.info('LAB_DetalleProtocolo actualizado para subItem:', idItem);
-                }
-            
-        } )
-        .error( function( err ){
-            logger.error( err );
-            logMessages(errMessage,logTime);
-        } ); 
+                });
+                logger.info(result.value)
+                logger.info('LAB_DetalleProtocolo actualizado para subItem:', idItem);
+            }
+
+        })
+        .error(function (err) {
+            logger.error(err);
+            logMessages(errMessage, logTime);
+        });
 }
 
-function hasProtocolsToSend(){
-    return sql.execute( {  
+function hasProtocolsToSend() {
+    return sql.execute({
         query: "SELECT count(*) as total FROM LAB_TempProtocoloEnvio WHERE equipo = @equipo",
         params: {
             equipo: {
@@ -150,12 +150,12 @@ function hasProtocolsToSend(){
                 val: config.analyzer,
             }
         }
-    } )
-    
+    })
+
 }
 
-function getNextProtocolToSend(){
-    return sql.execute( {  
+function getNextProtocolToSend() {
+    return sql.execute({
         query: "SELECT TOP 1 * FROM LAB_TempProtocoloEnvio WHERE equipo = @equipo",
         params: {
             equipo: {
@@ -163,22 +163,22 @@ function getNextProtocolToSend(){
                 val: config.analyzer,
             }
         }
-    } )
+    })
 }
 
-function removeLastProtocolSent(){
-    getNextProtocolToSend().then( function( results ) {
+function removeLastProtocolSent() {
+    getNextProtocolToSend().then(function (results) {
         for (var i = 0; i < results.length; i++) { // Always only 1 iteration
-            var protocol = results[i]; 
+            var protocol = results[i];
             removeProtocol(protocol.idTempProtocoloEnvio);
         }
-        }, function( err ) {
-            logger.error( "Something bad happened:", err );
-        } );
+    }, function (err) {
+        logger.error("Something bad happened:", err);
+    });
 }
 
-function removeProtocol(idTempProtocolo){
-    return sql.execute( {  
+function removeProtocol(idTempProtocolo) {
+    return sql.execute({
         query: "DELETE FROM LAB_TempProtocoloEnvio WHERE idTempProtocoloEnvio = @_id",
         params: {
             _id: {
@@ -186,23 +186,23 @@ function removeProtocol(idTempProtocolo){
                 val: idTempProtocolo,
             }
         }
-    } )
+    })
 }
 
 
-function logMessages(logMessage,logTime){
-    sql.execute( {  
+function logMessages(logMessage, logTime) {
+    sql.execute({
         query: "INSERT INTO Temp_Mensaje(mensaje,fechaRegistro) VALUES (@_mensaje,@_fechaRegistro)",
         params: {
-            _mensaje: { type: sql.NVARCHAR, val: logMessage},
-            _fechaRegistro: { type: sql.DATETIME, val: logTime},
+            _mensaje: { type: sql.NVARCHAR, val: logMessage },
+            _fechaRegistro: { type: sql.DATETIME, val: logTime },
         }
-    } ).then( function( results ) {
-        logger.info( results );
-    }, function( err ) {
-        logger.error( "Something bad happened:", err );
-    } );
-    
+    }).then(function (results) {
+        logger.info(results);
+    }, function (err) {
+        logger.error("Something bad happened:", err);
+    });
+
 }
 
 
--- protocol.js
+++ protocol.js
@@ -18,24 +18,23 @@ var port = null; // COM Port Communication
 
 init();
 
-function init(){
-    SerialPort.list(function(err, ports){
-        initPort(err,ports);
+function init() {
+    SerialPort.list(function (err, ports) {
+        initPort(err, ports);
     });
 }
 
-function initPort(err, ports){
+function initPort(err, ports) {
     var portNumber = ports[0].comName;
     port = new SerialPort(portNumber);
     port.on('open', handlePortOpen);
     port.on('close', handlePortClose);
     port.on('data', handlePortData);
-    port.on('error', function(err) {
-          logger.error(err.message);
-      throw new Error('Port Error: ' + err.message);
+    port.on('error', function (err) {
+        logger.error(err.message);
+        throw new Error('Port Error: ' + err.message);
     })
 }
-
 
 function handlePortOpen() {
     logger.info('Port open. Data rate: ' + port.options.baudRate);
@@ -48,78 +47,78 @@ function handlePortClose() {
     logger.info('Port Closed.');
 }
 
-function handlePortWrite(data){
+function handlePortWrite(data) {
     logger.info(data);
     port.write(data);
     initTimer();
 }
 
-function handlePortData(data){
+function handlePortData(data) {
     logger.info(data); // Raw Buffer Data
     var data = data.toString('ascii');
-     
-     if (isTransferState){
-         if (isClientMode){
-             readDataAsClient(data);
-         }
-         else{
-             readDataAsServer(data);
-         }
-     }
-     else{
-         readDataAsServer(data);
-     }
+
+    if (isTransferState) {
+        if (isClientMode) {
+            readDataAsClient(data);
+        }
+        else {
+            readDataAsServer(data);
+        }
+    }
+    else {
+        readDataAsServer(data);
+    }
 }
 
 ////////////////// SERVER MODE //////////////////////
 
 var inputChunks = [];
 
-function readDataAsServer(data){
+function readDataAsServer(data) {
     var response = '';
-    
-    if (data === token.ENQ){
+
+    if (data === token.ENQ) {
         logger.info('Request: ENQ');
-        if (!isTransferState){
+        if (!isTransferState) {
             isTransferState = true;
             response = token.ACK;
         }
-        else{
+        else {
             logger.error('ENQ is not expected. Transfer state already.');
             response = token.NAK;
         }
     }
-    else if (data === token.ACK){
+    else if (data === token.ACK) {
         logger.error('ACK is not expected.');
         throw new Error('ACK is not expected.');
     }
-    else if (data === token.NAK){
+    else if (data === token.NAK) {
         logger.error('NAK is not expected.');
         throw new Error('NAK is not expected.');
     }
-    else if (data === token.EOT){
-        if (isTransferState){
+    else if (data === token.EOT) {
+        if (isTransferState) {
             isTransferState = false;
             logger.info('EOT accepted. OK');
         }
-        else{
+        else {
             logger.error('Not ready to accept EOT message.');
             throw new Error('Not ready to accept EOT message.');
         }
     }
-    else if (data.startsWith(token.STX)){
-        if (!isTransferState){
+    else if (data.startsWith(token.STX)) {
+        if (!isTransferState) {
             discard_input_buffers();
             logger.error('Not ready to accept messages');
             response = token.NAK;
         }
-        else{
-            try{
+        else {
+            try {
                 logger.info('Accept message.Handling message');
                 handleMessage(data);
                 response = token.ACK;
             }
-            catch(err){
+            catch (err) {
                 logger.error('Error occurred on message handling.' + err)
                 response = token.NAK;
             }
@@ -129,35 +128,35 @@ function readDataAsServer(data){
         logger.error('Invalid data.');
         throw new Error('Invalid data.');
     }
-    
+
     handlePortWrite(response);
 };
 
-function handleMessage(message){
-    if (codec.isChunkedMessage(message)){
+function handleMessage(message) {
+    if (codec.isChunkedMessage(message)) {
         logger.debug('handleMessage: Is chunked transfer.');
         inputChunks.push(message);
     }
-    else if (typeof inputChunks !== 'undefined' && inputChunks.length > 0){
+    else if (typeof inputChunks !== 'undefined' && inputChunks.length > 0) {
         logger.debug('handleMessage: Previous chunks. This must be the last one');
         inputChunks.push(message);
-        dispatchMessage(inputChunks.join(''),token.ENCODING);
+        dispatchMessage(inputChunks.join(''), token.ENCODING);
         inputChunks = [];
     }
-    else{
+    else {
         logger.debug('handleMessage: Complete message. Dispatching');
-        dispatchMessage(message,token.ENCODING); 
+        dispatchMessage(message, token.ENCODING);
     }
 }
 
-function dispatchMessage(message){
+function dispatchMessage(message) {
     console.log(message);
     var records = codec.decodeMessage(message);
     logger.info(records);
     app.processResultRecords(records);
 }
 
-function discard_input_buffers(){
+function discard_input_buffers() {
     inputChunks = [];
 }
 
@@ -165,65 +164,65 @@ function discard_input_buffers(){
 
 ////////////////// CLIENT MODE //////////////////////
 
-var outputChunks = []; 
-var outputMessages = []; 
+var outputChunks = [];
+var outputMessages = [];
 var retryCounter = 0;
 var lastSendOk = false;
 var lastSendData = "";
 var timer;
 
-function readDataAsClient(data){
-    
-    if (data === token.ENQ){
-        if (lastSendData === token.ENQ){
+function readDataAsClient(data) {
+
+    if (data === token.ENQ) {
+        if (lastSendData === token.ENQ) {
             //TODO: Link Contention??
         }
         throw new Error('Client should not receive ENQ.');
     }
-    else if (data === token.ACK){
+    else if (data === token.ACK) {
         logger.debug('ACK Response');
         lastSendOk = true;
-        try{ 
+        try {
             sendMessage();
         }
-        catch(error){
+        catch (error) {
             logger.debug(error);
             closeClientSession();
         }
         //handlePortWrite(message); //self.push(message)
         // TODO: Revisar la condicion de abajo
         // if (message === token.EOT){
-            // self.openClientSession()
+        // self.openClientSession()
         // }
     }
-    else if (data === token.NAK){
+    else if (data === token.NAK) {
         // Handles NAK response from server.
 
         // The client tries to repeat last
         // send for allowed amount of attempts. 
         logger.debug('NAK Response');
-        if (lastSendData === token.ENQ){
+        if (lastSendData === token.ENQ) {
             openClientSession();
         }
-        else{
-            try{
+        else {
+            try {
                 lastSendOk = false;
                 sendMessage();
             }
-            catch(error){
+            catch (error) {
                 closeClientSession();
             }
         }
-        
+
         // TODO: Revisar la condicion de abajo
         // if message == EOT:
-            // self.openClientSession()
+        // self.openClientSession()
     }
-    else if (data === token.EOT){
+    else if (data === token.EOT) {
         isTransferState = false;
         throw new Error('Client should not receive EOT.');
     }
-    else if (data.startsWith(token.STX)){
+    else if (data.startsWith(token.STX)) {
         isTransferState = false;
         throw new Error('Client should not receive ASTM message.');
     }
@@ -232,71 +231,71 @@ function readDataAsClient(data){
     }
 }
 
-function prepareMessagesToSend(protocol){
+function prepareMessagesToSend(protocol) {
     outputMessages = [];
     outputMessages = app.composeOrderMessages(protocol);
 }
 
-function prepareNextEncodedMessage(){
+function prepareNextEncodedMessage() {
     outputChunks = [];
     outputChunks = codec.encode(outputMessages.shift());
 }
 
-function sendMessage(){
-    if (lastSendData === token.ENQ){
-        if (outputMessages.length > 0){
+function sendMessage() {
+    if (lastSendData === token.ENQ) {
+        if (outputMessages.length > 0) {
             // Still exists messages to send
             prepareNextEncodedMessage();
             sendData();
         }
-        else{
-            db.getNextProtocolToSend().then( function( results ) {
-            for (var i = 0; i < results.length; i++) { // Always only 1 iteration
-                var protocol = results[i]; 
-                prepareMessagesToSend(protocol)
-                prepareNextEncodedMessage();
-                sendData();
-            }
-            }, function( err ) {
-                logger.error( "Something bad happened:", err );
-            } );
+        else {
+            db.getNextProtocolToSend().then(function (results) {
+                for (var i = 0; i < results.length; i++) { // Always only 1 iteration
+                    var protocol = results[i];
+                    prepareMessagesToSend(protocol)
+                    prepareNextEncodedMessage();
+                    sendData();
+                }
+            }, function (err) {
+                logger.error("Something bad happened:", err);
+            });
         }
     }
-    else{
+    else {
         sendData();
     }
 }
 
-function sendData(){
-    if (!lastSendOk){
-        if (retryCounter > 6){
+function sendData() {
+    if (!lastSendOk) {
+        if (retryCounter > 6) {
             closeClientSession();
-            if (lastSendData !== token.ENQ){
+            if (lastSendData !== token.ENQ) {
                 // Remove last protocol to send to prevent future problems with 
                 // the same protocol
                 db.removeLastProtocolSent();
             }
             return;
         }
-        else{
+        else {
             retryCounter = retryCounter + 1;
         }
     }
-    else{
+    else {
         retryCounter = 0;
-        if (outputChunks.length > 0){
+        if (outputChunks.length > 0) {
             lastSendData = outputChunks.shift();
         }
-        else{
+        else {
             closeClientSession();
-            if (outputMessages.length > 0){
+            if (outputMessages.length > 0) {
                 openClientSession();
             }
-            else{
+            else {
                 db.removeLastProtocolSent();
                 // checkDataToSend();
             }
-            
+
             return;
         }
     }
@@ -304,14 +303,14 @@ function sendData(){
 }
 
 
-function openClientSession(){
+function openClientSession() {
     logger.info('Open Client Session');
     retryCounter = retryCounter + 1;
-    if (retryCounter > 6){
+    if (retryCounter > 6) {
         logger.error('Exceed number of retries');
         closeClientSession();
     }
-    else{
+    else {
         handlePortWrite(token.ENQ);
         lastSendData = token.ENQ;
         isTransferState = true;
@@ -319,7 +318,7 @@ function openClientSession(){
     }
 }
 
-function closeClientSession(){
+function closeClientSession() {
     logger.debug('Close Client Session');
     handlePortWrite(token.EOT);
     isTransferState = false;
@@ -327,41 +326,41 @@ function closeClientSession(){
     retryCounter = 0;
 }
 
-function checkDataToSend(){
-    db.hasProtocolsToSend().then( function( results ) {
-        if (results[0].total > 0){
+function checkDataToSend() {
+    db.hasProtocolsToSend().then(function (results) {
+        if (results[0].total > 0) {
             logger.info("Exist data to send");
-            if (!isClientMode){
+            if (!isClientMode) {
                 openClientSession();
             }
         }
-        else{
-            if (isClientMode){
-            isClientMode = false;
+        else {
+            if (isClientMode) {
+                isClientMode = false;
             }
-            else{
+            else {
                 return;
                 logger.info('Waiting for data to send');
             }
         }
-    }, function( err ) {
-        logger.error( "Something bad happened:", err );
-    } );
+    }, function (err) {
+        logger.error("Something bad happened:", err);
+    });
 }
 
-function initTimer(){
+function initTimer() {
     clearTimeout(timer);
-    timer = setTimeout(timeoutCommunication,5000);
+    timer = setTimeout(timeoutCommunication, 5000);
 }
 
-function timeoutCommunication(){
-    if (isTransferState){
+function timeoutCommunication() {
+    if (isTransferState) {
         throw new Error('Timeout Communication');
     }
 }
 
 function runIntervalCheck() {
-  setInterval(checkDataToSend, 10000);
+    setInterval(checkDataToSend, 10000);
 };
 
 
