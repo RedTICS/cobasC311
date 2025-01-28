e7eadfaebf565aa5b54c63d013e4d157f7c25ad8
Vanesa Rimada <80859025+vrimada@users.noreply.github.com>
2025-01-28 14:44:59 -0300

    Pruebas en equipo (#2)
    * fix: se adaptan las queries  para SIL 2
    
    Se adaptan las queries del archivo db.js para poder utilizar esta interfaz en el sistema Sil MultiEfector. Se debe indicar el idEfector en el archivo config.ts
    
    * fix: Correccion de Config.js
    
    * FIX: Se agregan logs para el test en el laboratorio de complejidad  (San Martin) y se corrige una consulta de SQL
    
    * Cambios realizados cuando se probo la interfaz con el equipo

--- list-ports.js
+++ list-ports.js
@@ -1,8 +1,25 @@
 var SerialPort = require('serialport');
 SerialPort.list(function (err, ports) {
-  ports.forEach(function(port) {
-    console.log(port.comName);
-    console.log(port.pnpId);
-    console.log(port.manufacturer);
+      ports.forEach(function(port) {
+      try{
+      console.log("Datos de puerto",port.comName);
+      //console.log("port.comName ",port.comName);
+      //console.log("port.pnpId",port.pnpId);
+      //console.log("port.manufacturer", port.manufacturer);
+      console.dir(port);
+
+      var portNumber = port.comName;
+      puerto = new SerialPort(portNumber);
+      console.log('Port open. Data rate: ' + puerto.options.baudRate);
+      console.log('Data Bits.: ' + puerto.options.dataBits);
+      console.log('Parity.: ' + puerto.options.parity);
+      console.log('Stop bit.: ' + puerto.options.stopBits);
+      console.log("---------------");
+      puerto.on('error', function (err) {
+        throw new Error('Port Error: ' + err.message);
+    })
+      }catch(error){
+        console.error(error);
+    }
   });
 });
